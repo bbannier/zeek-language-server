b7b07afd1f9b3d342ac14a0ec3558e4cc237e0fd
Benjamin Bannier <bbannier@users.noreply.github.com>
2022-01-11 18:45:47 +0100

    Scaffolding for main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,50 @@
-fn main() {}
+use {
+    clap::Parser,
+    clap_verbosity_flag::Verbosity,
+    eyre::{eyre, Result},
+    std::path::PathBuf,
+    tracing::info,
+    tracing_log::LogTracer,
+    tracing_subscriber::{layer::SubscriberExt, Registry},
+    tracing_tree::HierarchicalLayer,
+};
+
+#[derive(Parser, Debug)]
+#[clap(about, version)]
+struct Args {
+    #[clap(short, long, default_value = "/tmp")]
+    log_directory: PathBuf,
+
+    #[clap(flatten)]
+    verbose: Verbosity,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts = Args::parse();
+
+    let log_file =
+        tracing_appender::rolling::never(&opts.log_directory, "zeek-language-server.log");
+    let (log_writer, _guard) = tracing_appender::non_blocking(log_file);
+
+    LogTracer::init_with_filter(
+        opts.verbose
+            .log_level()
+            .ok_or_else(|| eyre!("invalid logging level"))?
+            .to_level_filter(),
+    )?;
+
+    tracing::subscriber::set_global_default(
+        Registry::default().with(
+            HierarchicalLayer::new(2)
+                .with_thread_ids(true)
+                .with_thread_names(true)
+                .with_writer(log_writer)
+                .with_ansi(true),
+        ),
+    )?;
+
+    info!("starting Zeek language server");
+
+    Ok(())
+}

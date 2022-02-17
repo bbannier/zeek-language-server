7fb07b3e77abfe5ae784e2bd3d4e55149da6ac90
Benjamin Bannier <bbannier@gmail.com>
2022-02-17 16:22:54 +0100

    Add vscode scaffolding to support downloading binaries

--- vscode/README.md
+++ vscode/README.md
@@ -2,7 +2,23 @@
 
 Provides support for zeek-language-server, a LSP server for Zeek script.
 
-## Quick start
+## Requirements
+
+This extension currently provides binaries for x86_64 Darwin and Linux
+platforms.[^other]
+
+A [Zeek](https://zeek.org) installation is required, and `zeek-config` needs to
+be in `PATH`.
+
+## Configuration
+
+This extension provides configuration through VSCode's configuration settings.
+All configuration settings are under `zeekLanguageServer.*`.
+
+- `zeekLanguageServer.path`: if set used to launch the language server
+  executable; if unset the executable is looked up in `PATH`.
+
+## Building from source
 
 This extension needs `zeek-language-server` to work. See [its
 documentation](https://github.com/bbannier/zeek-language-server) on
@@ -28,10 +44,10 @@ In a copy of this directory:
    code --install-extension zeek-language-server.vsix
    ```
 
-## Configuration
-
-This extension provides configuration through VSCode's configuration settings.
-All configuration settings are under `zeekLanguageServer.*`.
-
-- `zeekLanguageServer.path`: if set used to launch the language server
-  executable; if unset the executable is looked up in `PATH`.
+[^other]:
+    The underlying [language server
+    binary](https://github.com/bbannier/zeek-language-server) might be buildable
+    on other platforms, but needs to be provided out of band. If such a binary
+    exists elsewhere one currently needs to point `zeekLanguageServer.path` to
+    it as otherwise the extension will automatically attempt to download the
+    upstream binary (which will fail on unsupported platforms).
--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,4 +1,16 @@
-import { ExtensionContext, workspace } from "vscode";
+import { inspect } from "util";
+import got from "got";
+import {
+  ExtensionContext,
+  ProgressLocation,
+  Uri,
+  workspace,
+  window,
+} from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import * as crypto from "crypto";
+import * as stream from "stream";
 
 import {
   Executable,
@@ -6,15 +18,127 @@ import {
   LanguageClientOptions,
   ServerOptions,
 } from "vscode-languageclient/node";
+import { promisify } from "util";
 
-let client: LanguageClient;
+const TAG = "v0.1.0";
+const BASE_URL =
+  "https://github.com/bbannier/zeek-language-server/releases/download";
 
-export async function activate(context: ExtensionContext) {
+const PLATFORMS = {
+  linux: "x86_64-unknown-linux-gnu",
+  darwin: "x86_64-apple-darwin",
+};
+
+const exists = promisify(fs.exists);
+const pipeline = promisify(stream.pipeline);
+
+export const getServerDestination = async (
+  context: ExtensionContext,
+  tag: string
+): Promise<string> => {
+  const { globalStorageUri } = context;
+  const uri = Uri.joinPath(
+    globalStorageUri,
+    "server",
+    `zeek-language-server-${tag}`
+  );
+
+  const { fsPath } = uri;
+  await fs.promises.mkdir(path.dirname(fsPath), { recursive: true });
+  return fsPath;
+};
+
+const getServerUrl = async (tag: string): Promise<string> => {
+  const platform = process.platform;
+  const variant = PLATFORMS[platform];
+
+  if (variant) {
+    return `${BASE_URL}/${tag}/zeek-language-server-${variant}`;
+  } else {
+    log.error(`Unsupported platform ${platform}`);
+    return Promise.reject();
+  }
+};
+
+const getServerOrDownload = async (
+  context: ExtensionContext,
+  tag: string
+): Promise<string> => {
+  // FIXME(bbannier): check whether executable is somewhere in PATH.
+  const dest = await getServerDestination(context, tag);
+  if (!(await exists(dest))) {
+    const url = await getServerUrl(tag);
+    log.info(`Downloading ${url} to ${dest}`);
+    const tempDest = path.join(
+      path.dirname(dest),
+      `.tmp${crypto.randomBytes(5).toString("hex")}`
+    );
+
+    const params = {
+      title: "Downloading zeek-language-server binary",
+      location: ProgressLocation.Notification,
+      cancelable: false,
+    };
+
+    await window.withProgress(
+      params,
+      async (progressHandle, cancellationHandle) => {
+        let lastPercent = 0;
+
+        const stream = got.stream(url).on("downloadProgress", (progress) => {
+          const message = `${(progress.percent * 100).toFixed(0)}%`;
+          const increment = progress.percent - lastPercent;
+          progressHandle.report({ message, increment });
+          lastPercent = progress.percent;
+        });
+
+        cancellationHandle.onCancellationRequested(stream.destroy.bind(stream));
+
+        await pipeline(stream, fs.createWriteStream(tempDest, { mode: 0o755 }));
+      }
+    );
+
+    await fs.promises.rename(tempDest, dest);
+  }
+
+  return dest;
+};
+
+const log = new (class {
+  private readonly output = window.createOutputChannel("zeek-language-server");
+
+  info(...msg: [unknown, ...unknown[]]): void {
+    log.write("INFO", ...msg);
+  }
+
+  error(...msg: [unknown, ...unknown[]]): void {
+    log.write("ERROR", ...msg);
+    log.output.show(true);
+  }
+
+  private write(label: string, ...messageParts: unknown[]): void {
+    const message = messageParts.map(log.stringify).join(" ");
+    const dateTime = new Date().toLocaleString();
+    log.output.appendLine(`${label} [${dateTime}]: ${message}`);
+  }
+
+  private stringify(val: unknown): string {
+    if (typeof val === "string") return val;
+    return inspect(val, {
+      colors: false,
+      depth: 6, // heuristic
+    });
+  }
+})();
+
+let CLIENT: LanguageClient;
+
+export async function activate(context: ExtensionContext): Promise<void> {
   const config = workspace.getConfiguration("zeekLanguageServer");
   const userDefinedServerPath = config.get<string>("path");
   const serverPath =
     userDefinedServerPath == ""
-      ? "zeek-language-server"
+      ? await getServerOrDownload(context, TAG)
       : userDefinedServerPath;
 
   const serverExecutable: Executable = {
@@ -30,17 +154,17 @@ export async function activate(context: ExtensionContext) {
     documentSelector: [{ scheme: "file", language: "zeek" }],
   };
 
-  client = new LanguageClient(
+  CLIENT = new LanguageClient(
     "zeek-language-server",
     "Zeek Language Server",
     serverOptions,
     clientOptions
   );
-  client.start();
+  CLIENT.start();
 }
 
 export async function deactivate(): Promise<void> {
-  if (client) {
-    await client.stop();
+  if (CLIENT) {
+    await CLIENT.stop();
   }
 }

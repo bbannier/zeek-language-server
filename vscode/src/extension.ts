af01ff62c4d8f7db346558c4060a0b075bd7bd89
Benjamin Bannier <bbannier@gmail.com>
2024-03-10 22:04:23 +0100

    Import Node APIs as `node:`

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -9,9 +9,9 @@ import {
   commands,
   env,
 } from "vscode";
-import * as fs from "fs";
-import * as path from "path";
-import * as child_process from "child_process";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as child_process from "node:child_process";
 
 import {
   Executable,

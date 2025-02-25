9c9360a5d06d0a98db7cc2731de7589207ed731d
Benjamin Bannier <bbannier@gmail.com>
2025-02-25 12:52:04 +0100

    Fix markdowninfication of some external rst links

--- src/rst.rs
+++ src/rst.rs
@@ -22,7 +22,7 @@ pub fn markdownify(rst: &str) -> Str {
 
 fn external_link(rst: &str) -> Cow<str> {
     static RE: LazyLock<regex::Regex> = LazyLock::new(|| {
-        regex::Regex::new(r"`(\S+(\s+\S+)*)\s+<(.*)>`__").expect("invalid regexp")
+        regex::Regex::new(r"`(\S+(\s+\S+)*)\s+<(.*)>`_{1,2}").expect("invalid regexp")
     });
 
     RE.replace_all(rst, "[$1]($3)")
@@ -123,6 +123,13 @@ mod test {
         assert_eq!(
             markdownify("See `Wikipedia article <http://en.wikipedia.org/wiki/Levenshtein_distance>`__ for more information."),
             "See [Wikipedia article](http://en.wikipedia.org/wiki/Levenshtein_distance) for more information."
+        );
+
+        assert_eq!(
+            markdownify(
+                "See ZeroMQ's `ZMQ_LINGER documentation <http://api.zeromq.org/4-2:zmq-setsockopt#toc24>`_ for more details."
+            ),
+            "See ZeroMQ's [ZMQ_LINGER documentation](http://api.zeromq.org/4-2:zmq-setsockopt#toc24) for more details."
         );
     }
 
